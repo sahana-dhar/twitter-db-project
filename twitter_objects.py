aa18ef0ad5f216449a80a1522061e537e536163c
anyawild <wild.an@northeastern.edu>
2026-01-20 12:58:17 -0500

    objects

--- twitter_objects.py
+++ twitter_objects.py
@@ -1,1 +1,20 @@
 ## object classes
+
+
+class Tweet:
+
+    def __init__(self, tweet_id, user_id, tweet_ts, tweet_text):
+        self.tweet_id = tweet_id
+        self.user_id = user_id
+        self.tweet_ts = tweet_ts
+        self.tweet_text = tweet_text
+
+class Follows:
+
+    def __init__(self, follower_id, followee_id):
+        self.follower_id = follower_id
+        self.followee_id = followee_id
+
+    # def __str__(self):
+    #     return f"Doctor: {self.first} {self.last} ({self.specialty})"
+
