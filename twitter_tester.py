8661e9a01b75c865cddea05afa1f576730e44de8
Sahana Dhar <sahanadhar@Sahanas-MacBook-Air.local>
2026-01-21 15:19:49 -0500

    added rate print statements

--- twitter_tester.py
+++ twitter_tester.py
@@ -20,16 +20,14 @@ def main():
     Test 1: How many tweets can we post per second?
     """
     # use sample tweets file
-    csv_file = 'tweet.csv'
+    csv_file = 'tweets_sample.csv'
     count_tweets = 0 # starting count
 
     # open file and post as many tweets as possible
     with open(csv_file, 'r', encoding='utf-8') as f:
         next(f)
-
-        # start timer for test 2
+        # start timer for test 1
         start_time = time.time()
-
         # for one second, post as many tweets as possible
         for line in f:
             if time.time() - start_time <= 1:
@@ -39,7 +37,8 @@ def main():
             else:
                 break
 
-    print(f'Tweets per second is: {count_tweets}')
+    print(f'tweets posted: {count_tweets}')
+    print(f'rate: {count_tweets} tweets/sec')
 
     """
     Test 2: How many timelines can we retrieve per second?
@@ -47,18 +46,17 @@ def main():
     # list of all users to select from randomly
     all_users = api.getAllUsers()
     count_timelines = 0 # start count
-
-    # start timer for test 1
+    # start timer for test 2
     start_time = time.time()
 
     # for one second, retrieve as many timelines as possible
     while time.time() - start_time <= 1:
-        random_user = random.choice(all_users) # select random user
+        random_user = random.choice(all_users)  # select random user
         tweets_timeline = api.getHomeTimeline(random_user)
-        count_timelines+=1 
+        count_timelines+= 1
 
-    print(f'Timelines per second is: {count_timelines}')
-
+    print(f'timelines retrieved: {count_timelines}')
+    print(f'rate: {count_timelines} timelines/sec')
 
 if __name__ == '__main__':
     main()
