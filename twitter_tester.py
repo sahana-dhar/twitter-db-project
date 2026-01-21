659dffca0a93d0fa1b2bd9caaf5a7c6f4b8ce5f4
anyawild <wild.an@northeastern.edu>
2026-01-21 13:47:50 -0500

    tester file

--- twitter_tester.py
+++ twitter_tester.py
@@ -1,1 +1,26 @@
-## the two separate programs
+## testing the two separate functions
+
+
+import os
+from twitter_api import TwitterAPI
+from twitter_objects import Tweet, Follows
+
+def main():
+
+    # Authenticate
+    api = TwitterAPI()
+
+    ex_user = api.getAllUsers()[0]
+
+    # Get home timeline
+    tweets_timeline = api.getHomeTimeline(ex_user)
+    for t in tweets_timeline:
+        print(t)
+
+    # post new tweet
+    twee = Tweet(ex_user, "hello tweet world")
+    api.postTweet(twee)
+
+
+if __name__ == '__main__':
+    main()
