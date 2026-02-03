524d581e4cc1e2df26054b35774739da56e2a482
anyawild <wild.an@northeastern.edu>
2026-02-03 16:39:10 -0500

    comments

--- create_db_redis.py
+++ create_db_redis.py
@@ -1,18 +1,31 @@
+"""
+filename: create_db_redis.py
+
+description: Setup script for REDIS Twitter database. Creates FOLLOWS table.
+
+Written by Sahana
+"""
+
 import redis
 import csv
 
 r = redis.Redis(decode_responses=True)
-# clear existing data 
-r.flushdb()
+r.flushdb() # clear existing data 
 
+# load follows data from csv
 with open("follows_sample.csv") as f:
     reader = csv.reader(f)
     next(reader)  # skip header
     
     count = 0
     for follower_id, followee_id in reader:
+        
+        # create a followers set for each followee_id in Redis
         r.sadd(f"followers:{followee_id}", follower_id)
-        r.sadd(f"following:{follower_id}", followee_id)  
+        
+        ## remove? - r.sadd(f"following:{follower_id}", followee_id)  - not used in api, only follower is
+
+        # add follower_id/followee_id to a users set, Redis automatically removes duplicates
         r.sadd("users", follower_id)
         r.sadd("users", followee_id)
         count += 1
